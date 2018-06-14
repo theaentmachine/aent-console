b37094c1993be83573f701486bfe5a690470fb84
Jindun SHAO <sjindun@gmail.com>
2018-06-14 11:52:58 +0200

    refactoring and adding adders

--- src/Service/Service.php
+++ src/Service/Service.php
@@ -9,21 +9,21 @@ use TheAentMachine\Service\Exception\ServiceException;
 class Service implements \JsonSerializable
 {
     /** @var string */
-    protected $serviceName;
+    protected $serviceName = '';
     /** @var string */
-    protected $image;
-    /** @var int[]|string[] */
-    protected $internalPorts;
+    protected $image = '';
+    /** @var int[] */
+    protected $internalPorts = [];
     /** @var string[] */
-    protected $dependsOn;
+    protected $dependsOn = [];
     /** @var mixed[] */
-    protected $ports;
+    protected $ports = [];
     /** @var mixed[] */
-    protected $labels;
+    protected $labels = [];
     /** @var mixed[] */
-    protected $environment;
+    protected $environment = [];
     /** @var mixed[] */
-    protected $volumes;
+    protected $volumes = [];
     /** @var \stdClass */
     protected $validatorSchema;
 
@@ -32,14 +32,6 @@ class Service implements \JsonSerializable
      */
     public function __construct()
     {
-        $this->serviceName = '';
-        $this->image = '';
-        $this->internalPorts = array();
-        $this->dependsOn = array();
-        $this->ports = array();
-        $this->labels = array();
-        $this->environment = array();
-        $this->volumes = array();
         $this->validatorSchema = json_decode(file_get_contents(__DIR__ . '/ServiceJsonSchema.json'), false);
     }
 
@@ -67,27 +59,6 @@ class Service implements \JsonSerializable
     }
 
     /**
-     * @param \stdClass|array|string $data
-     * @param bool $throwsException
-     * @return bool
-     * @throws ServiceException
-     */
-    public function checkValidity($data, bool $throwsException = true): bool
-    {
-        if (\is_array($data)) {
-            $data = json_decode(json_encode($data), false);
-        }
-        $validator = new Validator();
-        $result = $validator->dataValidation($data, $this->validatorSchema);
-        if ($throwsException && !$result->isValid()) {
-            /** @var ValidationError $vError */
-            $vError = $result->getFirstError();
-            throw ServiceException::invalidServiceData($vError);
-        }
-        return $result->isValid();
-    }
-
-    /**
      * Specify data which should be serialized to JSON
      * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
      * @return array data which can be serialized by <b>json_encode</b>,
@@ -97,7 +68,7 @@ class Service implements \JsonSerializable
      */
     public function jsonSerialize(): array
     {
-        $array = Utils::arrayFilterRec(array(
+        $array = self::arrayFilterRec(array(
             'serviceName' => $this->serviceName,
             'service' => array(
                 'image' => $this->image,
@@ -113,6 +84,166 @@ class Service implements \JsonSerializable
         return $array;
     }
 
+    /**
+     * @param \stdClass|array|string $data
+     * @return bool
+     * @throws ServiceException
+     */
+    private function checkValidity($data): bool
+    {
+        if (\is_array($data)) {
+            $data = json_decode(json_encode($data), false);
+        }
+        $validator = new Validator();
+        $result = $validator->dataValidation($data, $this->validatorSchema);
+        if (!$result->isValid()) {
+            /** @var ValidationError $vError */
+            $vError = $result->getFirstError();
+            throw ServiceException::invalidServiceData($vError);
+        }
+        return $result->isValid();
+    }
+
+    /**
+     * Delete all key/value pairs with empty value by recursively using array_filter
+     * @param array $input
+     * @return mixed[] array
+     */
+    private static function arrayFilterRec(array $input): array
+    {
+        foreach ($input as &$value) {
+            if (\is_array($value)) {
+                $value = self::arrayFilterRec($value);
+            }
+        }
+        return array_filter($input);
+    }
+
+    /**
+     * @return string
+     */
+    public function getServiceName(): string
+    {
+        return $this->serviceName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    /**
+     * @return int[]
+     */
+    public function getInternalPorts(): array
+    {
+        return $this->internalPorts;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getDependsOn(): array
+    {
+        return $this->dependsOn;
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function getPorts(): array
+    {
+        return $this->ports;
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function getLabels(): array
+    {
+        return $this->labels;
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function getEnvironment(): array
+    {
+        return $this->environment;
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function getVolumes(): array
+    {
+        return $this->volumes;
+    }
+
+    /**
+     * @param int $internalPort
+     */
+    public function addInternalPort(int $internalPort): void
+    {
+        $this->internalPorts[] = $internalPort;
+    }
+
+    /**
+     * @param string $dependsOn
+     */
+    public function addDependsOn(string $dependsOn): void
+    {
+        $this->dependsOn[] = $dependsOn;
+    }
+
+    /**
+     * @param mixed $port
+     */
+    public function addPort(array $port): void
+    {
+        $this->ports[] = array(
+            'source' => $port['source'] ?? '',  //optional
+            'target' => $port['target'],
+        );
+    }
+
+    /**
+     * @param mixed $label
+     */
+    public function addLabel(array $label): void
+    {
+        $this->labels[] = array(
+            'key' => $label['key'],
+            'values' => $label['values'] ?? '', //optional
+        );
+    }
+
+    /**
+     * @param mixed $environment
+     */
+    public function addEnvironment(array $environment): void
+    {
+        $this->environment[] = array(
+            'key' => $environment['key'],
+            'values' => $environment['values'] ?? '', //optional
+        );
+    }
+
+    /**
+     * @param mixed $volume
+     */
+    public function addVolume(array $volume): void
+    {
+        $this->volumes[] = array(
+            'type' => $volume['type'],
+            'source' => $volume['source'],
+            'target' => $volume['target'],
+            'readOnly' => $volume['readOnly'] ?? null,
+        );
+    }
 
     /**
      * @param string $serviceName
@@ -135,7 +266,7 @@ class Service implements \JsonSerializable
     }
 
     /**
-     * @param int[]|string[] $internalPorts
+     * @param int[] $internalPorts
      * @return Service
      */
     public function setInternalPorts(array $internalPorts): Service
@@ -155,7 +286,7 @@ class Service implements \JsonSerializable
     }
 
     /**
-     * @param string[] $ports
+     * @param mixed[] $ports
      * @return Service
      */
     public function setPorts(array $ports): Service
@@ -165,7 +296,7 @@ class Service implements \JsonSerializable
     }
 
     /**
-     * @param string[] $labels
+     * @param mixed[] $labels
      * @return Service
      */
     public function setLabels(array $labels): Service
@@ -175,7 +306,7 @@ class Service implements \JsonSerializable
     }
 
     /**
-     * @param string[] $environment
+     * @param mixed[] $environment
      * @return Service
      */
     public function setEnvironment(array $environment): Service
@@ -185,7 +316,7 @@ class Service implements \JsonSerializable
     }
 
     /**
-     * @param string[] $volumes
+     * @param mixed[] $volumes
      * @return Service
      */
     public function setVolumes(array $volumes): Service
@@ -193,4 +324,5 @@ class Service implements \JsonSerializable
         $this->volumes = $volumes;
         return $this;
     }
+
 }
