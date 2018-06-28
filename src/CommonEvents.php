04fb06fbb521cc4cad238aa7ef40bc8d15f84ba6
Jindun SHAO <sjindun@gmail.com>
2018-06-28 16:21:16 +0200

    adding NEW_IMAGE handling functions

--- src/CommonEvents.php
+++ src/CommonEvents.php
@@ -14,6 +14,7 @@ class CommonEvents
 {
     private const NEW_SERVICE = 'NEW_SERVICE';
     private const NEW_VIRTUAL_HOST = 'NEW_VIRTUAL_HOST';
+    private const NEW_IMAGE = 'NEW_IMAGE';
 
     /**
      * @throws CannotHandleEventException
@@ -102,6 +103,47 @@ class CommonEvents
                 Hermes::setDependencies(['theaentmachine/aent-traefik']);
             } else {
                 throw CannotHandleEventException::cannotHandleEvent(self::NEW_VIRTUAL_HOST);
+            }
+        }
+    }
+
+    /**
+     * @throws CannotHandleEventException
+     */
+    public function dispatchImage(Service $service, QuestionHelper $helper, InputInterface $input, OutputInterface $output): void
+    {
+        $this->canDispatchImageOrFail($helper, $input, $output);
+
+        Hermes::dispatchJson(self::NEW_IMAGE, $service);
+    }
+
+    /**
+     * @throws CannotHandleEventException
+     */
+    public function canDispatchImageOrFail(QuestionHelper $helper, InputInterface $input, OutputInterface $output): void
+    {
+        $canHandle = Hermes::canHandleEvent(self::NEW_IMAGE);
+
+        if (!$canHandle) {
+            $output->writeln('<error>Heads up!</error>');
+            $output->writeln('It seems that Aenthill does not know how to handle the creation of a new image. You need to install a dedicated Aent for this.');
+            $output->writeln('Most of the time, you want to put the commands in a Dockerfile. We have a pretty good Aent for this: <info>theaentmachine/aent-dockerfile</info>.');
+            $question = new Question('Do you want me to add this Aent for you? (y/n) ', 'y');
+            $question->setValidator(function (string $value) {
+                $value = \strtolower(trim($value));
+
+                if ($value !== 'y' && $value !== 'n') {
+                    throw new \InvalidArgumentException('Please type "y" or "n"');
+                }
+
+                return $value;
+            });
+            $answer = $helper->ask($input, $output, $question);
+
+            if ($answer === 'y') {
+                Hermes::setDependencies(['theaentmachine/aent-dockerfile']);
+            } else {
+                throw CannotHandleEventException::cannotHandleEvent(self::NEW_IMAGE);
             }
         }
     }
