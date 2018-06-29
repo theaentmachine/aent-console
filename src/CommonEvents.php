0db5f9b040bd7029511942589dccb724eba5b656
Jindun SHAO <sjindun@gmail.com>
2018-06-29 09:56:48 +0200

    changing symfony helper to AentHelper

--- src/CommonEvents.php
+++ src/CommonEvents.php
@@ -16,38 +16,48 @@ class CommonEvents
     private const NEW_VIRTUAL_HOST = 'NEW_VIRTUAL_HOST';
     private const NEW_IMAGE = 'NEW_IMAGE';
 
+    /** @var AentHelper */
+    private $aentHelper;
+    /** @var OutputInterface */
+    private $output;
+
+    /**
+     * CommonEvents constructor.
+     * @param AentHelper $aentHelper
+     * @param OutputInterface $output
+     */
+    public function __construct(AentHelper $aentHelper, OutputInterface $output)
+    {
+        $this->aentHelper = $aentHelper;
+        $this->output = $output;
+    }
+    
     /**
      * @throws CannotHandleEventException
      */
-    public function dispatchService(Service $service, QuestionHelper $helper, InputInterface $input, OutputInterface $output): void
+    public function dispatchService(Service $service): void
     {
-        $this->canDispatchServiceOrFail($helper, $input, $output);
-
+        $this->canDispatchServiceOrFail();
         Hermes::dispatchJson(self::NEW_SERVICE, $service);
     }
 
     /**
      * @throws CannotHandleEventException
      */
-    public function canDispatchServiceOrFail(QuestionHelper $helper, InputInterface $input, OutputInterface $output): void
+    public function canDispatchServiceOrFail(): void
     {
         $canHandle = Hermes::canHandleEvent(self::NEW_SERVICE);
 
         if (!$canHandle) {
-            $output->writeln('<error>Heads up!</error>');
-            $output->writeln('It seems that Aenthill does not know how or where to store this new service. You need to install a dedicated Aent for this.');
-            $output->writeln('Most of the time, you want to put this service in a docker-compose.yml file. We have a pretty good Aent for this: <info>theaentmachine/aent-docker-compose</info>.');
-            $question = new Question('Do you want me to add this Aent for you? (y/n) ', 'y');
-            $question->setValidator(function (string $value) {
-                $value = \strtolower(trim($value));
+            $this->output->writeln('<error>Heads up!</error>');
+            $this->output->writeln('It seems that Aenthill does not know how or where to store this new service. You need to install a dedicated Aent for this.');
+            $this->output->writeln('Most of the time, you want to put this service in a docker-compose.yml file. We have a pretty good Aent for this: <info>theaentmachine/aent-docker-compose</info>');
 
-                if ($value !== 'y' && $value !== 'n') {
-                    throw new \InvalidArgumentException('Please type "y" or "n"');
-                }
-
-                return $value;
-            });
-            $answer = $helper->ask($input, $output, $question);
+            $answer = $this->aentHelper
+                ->question('Do you want me to add this Aent for you?')
+                ->yesNoQuestion()
+                ->setDefault('y')
+                ->ask();
 
             if ($answer === 'y') {
                 Hermes::setDependencies(['theaentmachine/aent-docker-compose']);
@@ -61,9 +71,9 @@ class CommonEvents
      * @throws CannotHandleEventException
      * @return array[] Returns the responses
      */
-    public function dispatchNewVirtualHost(QuestionHelper $helper, InputInterface $input, OutputInterface $output, string $serviceName, int $virtualPort = 80, string $virtualHost = null): ?array
+    public function dispatchNewVirtualHost(string $serviceName, int $virtualPort = 80, string $virtualHost = null): ?array
     {
-        $this->canDispatchVirtualHostOrFail($helper, $input, $output);
+        $this->canDispatchVirtualHostOrFail();
 
         $message = [
             'service' => $serviceName,
@@ -79,25 +89,20 @@ class CommonEvents
     /**
      * @throws CannotHandleEventException
      */
-    public function canDispatchVirtualHostOrFail(QuestionHelper $helper, InputInterface $input, OutputInterface $output): void
+    public function canDispatchVirtualHostOrFail(): void
     {
         $canHandle = Hermes::canHandleEvent(self::NEW_VIRTUAL_HOST);
 
         if (!$canHandle) {
-            $output->writeln('<error>Heads up!</error>');
-            $output->writeln('It seems that Aenthill does not know how to bind your container to a domain name. You need to install a reverse proxy for this.');
-            $output->writeln('Traefik is a good reverse proxy. We have an Aent to add Traefik to your project: <info>theaentmachine/aent-traefik</info>.');
-            $question = new Question('Do you want me to add this Aent for you? (y/n) ', 'y');
-            $question->setValidator(function (string $value) {
-                $value = \strtolower(trim($value));
+            $this->output->writeln('<error>Heads up!</error>');
+            $this->output->writeln('It seems that Aenthill does not know how to bind your container to a domain name. You need to install a reverse proxy for this.');
+            $this->output->writeln('Traefik is a good reverse proxy. We have an Aent to add Traefik to your project: <info>theaentmachine/aent-traefik</info>.');
 
-                if ($value !== 'y' && $value !== 'n') {
-                    throw new \InvalidArgumentException('Please type "y" or "n"');
-                }
-
-                return $value;
-            });
-            $answer = $helper->ask($input, $output, $question);
+            $answer = $this->aentHelper
+                ->question('Do you want me to add this Aent for you?')
+                ->yesNoQuestion()
+                ->setDefault('y')
+                ->ask();
 
             if ($answer === 'y') {
                 Hermes::setDependencies(['theaentmachine/aent-traefik']);
@@ -110,9 +115,9 @@ class CommonEvents
     /**
      * @throws CannotHandleEventException
      */
-    public function dispatchImage(Service $service, QuestionHelper $helper, InputInterface $input, OutputInterface $output): void
+    public function dispatchImage(Service $service): void
     {
-        $this->canDispatchImageOrFail($helper, $input, $output);
+        $this->canDispatchImageOrFail();
 
         Hermes::dispatchJson(self::NEW_IMAGE, $service);
     }
@@ -120,25 +125,20 @@ class CommonEvents
     /**
      * @throws CannotHandleEventException
      */
-    public function canDispatchImageOrFail(QuestionHelper $helper, InputInterface $input, OutputInterface $output): void
+    public function canDispatchImageOrFail(): void
     {
         $canHandle = Hermes::canHandleEvent(self::NEW_IMAGE);
 
         if (!$canHandle) {
-            $output->writeln('<error>Heads up!</error>');
-            $output->writeln('It seems that Aenthill does not know how to handle the creation of a new image. You need to install a dedicated Aent for this.');
-            $output->writeln('Most of the time, you want to put the instructions in a Dockerfile. We have a pretty good Aent for this: <info>theaentmachine/aent-dockerfile</info>.');
-            $question = new Question('Do you want me to add this Aent for you? (y/n) ', 'y');
-            $question->setValidator(function (string $value) {
-                $value = \strtolower(trim($value));
+            $this->output->writeln('<error>Heads up!</error>');
+            $this->output->writeln('It seems that Aenthill does not know how to handle the creation of a new image. You need to install a dedicated Aent for this.');
+            $this->output->writeln('Most of the time, you want to put the instructions in a Dockerfile. We have a pretty good Aent for this: <info>theaentmachine/aent-dockerfile</info>.');
 
-                if ($value !== 'y' && $value !== 'n') {
-                    throw new \InvalidArgumentException('Please type "y" or "n"');
-                }
-
-                return $value;
-            });
-            $answer = $helper->ask($input, $output, $question);
+            $answer = $this->aentHelper
+                ->question('Do you want me to add this Aent for you?')
+                ->yesNoQuestion()
+                ->setDefault('y')
+                ->ask();
 
             if ($answer === 'y') {
                 Hermes::setDependencies(['theaentmachine/aent-dockerfile']);
