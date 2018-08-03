08f83cb64d848955f92d999ef13774037aca64d6
Jindun SHAO <sjindun@gmail.com>
2018-08-03 11:39:22 +0200

    Add KUBERNETES_DIRNAME_KEY

--- src/Aenthill/CommonMetadata.php
+++ src/Aenthill/CommonMetadata.php
@@ -7,6 +7,7 @@ final class CommonMetadata
     public const ENV_NAME_KEY = 'ENV_NAME';
     public const ENV_TYPE_KEY = 'ENV_TYPE';
     public const DOCKER_COMPOSE_FILENAME_KEY = 'DOCKER_COMPOSE_FILENAME';
+    public const KUBERNETES_DIRNAME_KEY = 'KUBERNETES_DIRNAME';
     public const DOCKERFILE_NAME_KEY = 'DOCKERFILE_NAME';
 
     // common values...
