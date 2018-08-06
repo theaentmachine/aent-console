09c4386ab8b3ea3039788c5a089c98cb838e93a2
David Négrier <d.negrier@thecodingmachine.com>
2018-08-06 09:55:14 +0200

    Adding the ability to dump YAML with comments

---
+++ src/Yaml/CommentedItem.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace TheAentMachine\Yaml;
+
+
+class CommentedItem
+{
+    /**
+     * @var mixed
+     */
+    private $item;
+    /**
+     * @var string
+     */
+    private $comment;
+
+    /**
+     * @param mixed $item
+     * @param string $comment
+     */
+    public function __construct($item, string $comment)
+    {
+
+        $this->item = $item;
+        $this->comment = $comment;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getItem()
+    {
+        return $this->item;
+    }
+
+    /**
+     * @return string
+     */
+    public function getComment(): string
+    {
+        return $this->comment;
+    }
+}
