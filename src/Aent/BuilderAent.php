c468a50b0efc57f17da3e3fa9f6cf298aa0066b1
Julien Neuhart <j.neuhart@thecodingmachine.com>
2018-09-19 10:09:33 +0200

    finishing builder aent

--- src/Aent/BuilderAent.php
+++ src/Aent/BuilderAent.php
@@ -2,6 +2,17 @@
 
 namespace TheAentMachine\Aent;
 
+use TheAentMachine\Aent\Event\Builder\AbstractNewImageEvent;
+
 final class BuilderAent extends AbstractAent
 {
+    /**
+     * BuilderAent constructor.
+     * @param AbstractNewImageEvent $newImageEvent
+     */
+    public function __construct(AbstractNewImageEvent $newImageEvent)
+    {
+        parent::__construct();
+        $this->add($newImageEvent);
+    }
 }
